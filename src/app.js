276dc434c176b1231775d8f7c7b80d26741882df
jafow <jared.a.fowler@gmail.com>
2016-07-10 00:51:18 -0700

    render column headers statically. use .js extension

---
+++ src/app.js
@@ -0,0 +1,160 @@
+import React, { Component } from 'react'
+import { render } from 'react-dom'
+import { Table, Column, Cell } from 'fixed-data-table'
+
+const ads = {
+  "ads": [
+    {
+      "id": 1,
+      "remote_id": "123",
+      "name": "123",
+      "status": "ACTIVE",
+    },
+    {
+      "id": 2,
+      "remote_id": "456",
+      "name": "456",
+      "status": "ACTIVE",
+    },
+    {
+      "id": 3,
+      "remote_id": "789",
+      "name": "789",
+      "status": "ACTIVE",
+    },
+    {
+      "id": 4,
+      "remote_id": "901",
+      "name": "901",
+      "status": "ACTIVE",
+    },
+  ]
+}
+
+
+const metrics = {
+ "column_names": [
+      "impressions",
+      "reach",
+      "frequency",
+      "cpm",
+      "spend",
+      "ctr",
+      "cost_per_inline_link_click",
+      "actions:goal",
+      "actions:link_click",
+      "cost_per_action_type:cost_per_goal",
+      "actions:offsite_conversion"
+    ],
+ "rows": [
+      {
+        "remote_id": "123",
+        "impressions": "123",
+        "reach": 123,
+        "frequency": 1.0413449389302,
+        "cpm": 12.30131445905,
+        "spend": 182.49,
+        "ctr": 0.87630603303,
+        "cost_per_inline_link_click": 3.0415,
+        "actions:goal": 3,
+        "actions:link_click": 60,
+        "cost_per_action_type:cost_per_goal": 60.83,
+        "actions:offsite_conversion": 123
+      },
+      {
+        "remote_id": "456",
+        "impressions": "123",
+        "reach": 123,
+        "frequency": 1.0413449389302,
+        "cpm": 12.30131445905,
+        "spend": 182.49,
+        "ctr": 0.87630603303,
+        "cost_per_inline_link_click": 3.0415,
+        "actions:goal": 3,
+        "actions:link_click": 60,
+        "cost_per_action_type:cost_per_goal": 60.83,
+        "actions:offsite_conversion": 456
+      },
+      {
+        "remote_id": "789",
+        "impressions": "123",
+        "reach": 123,
+        "frequency": 1.0413449389302,
+        "cpm": 12.30131445905,
+        "spend": 182.49,
+        "ctr": 0.87630603303,
+        "cost_per_inline_link_click": 3.0415,
+        "actions:goal": 3,
+        "actions:link_click": 60,
+        "cost_per_action_type:cost_per_goal": 60.83,
+        "actions:offsite_conversion": 789
+      },
+      {
+        "remote_id": "901",
+        "impressions": "123",
+        "reach": 123,
+        "frequency": 1.0413449389302,
+        "cpm": 12.30131445905,
+        "spend": 182.49,
+        "ctr": 0.87630603303,
+        "cost_per_inline_link_click": 3.0415,
+        "actions:goal": 3,
+        "actions:link_click": 60,
+        "cost_per_action_type:cost_per_goal": 60.83,
+        "actions:offsite_conversion": 901
+      }
+    ]
+
+}
+
+//const FixedColumn = ({header
+
+
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      remoteIds: ads.ads
+    }
+  }
+
+  componentDidMount() {
+    // do request for data from /ads and /ads_metrics route
+  }
+
+  render() {
+    let columns = metrics.column_names.map((col, i) => (
+      <Column
+      header={col}
+      columnKey={i}
+      width={100}
+      />
+    ))
+
+    let cells = this.state.remoteIds.map((id, i) => (
+      <Cell columnKey={4} rowIndex={i}>{id.remote_id}</Cell>
+      ))
+  //  {columnKey, rowIndex, ...rest} = this.props
+
+    return (
+      <Table
+        header={this.props.header}
+        width={500}
+        maxHeight={5000}
+        rowsCount={this.state.remoteIds.length}
+        rowHeight={100}
+        headerHeight={100}
+    
+        >
+        { columns }
+      </Table>
+    )
+  }
+}
+
+render(
+  <App
+    header={'Ad IDs'}
+  />,
+  document.getElementById('app')
+  )
