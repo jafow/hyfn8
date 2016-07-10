a9f7f9b1065c006a3537afcfb2070c9691f12324
jafow <jared.a.fowler@gmail.com>
2016-07-09 22:56:45 -0700

    Add webpack

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path')
+
+
+module.exports = {
+  entry: path.join(__dirname, 'src/app.jsx'),
+  output: {
+    path: path.join(__dirname, 'client/'),
+    filename: 'bundle.js'
+  },
+  module: {
+      loaders: [
+        {
+          test: /jsx?$/,
+          exclude: /node_modules/,
+          loader: 'babel-loader',
+          query: {
+            presets: ['es2015', 'react']
+          }
+        }
+      ]
+  }
+}
