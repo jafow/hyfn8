df52e390f27b06a808c41752c2bbdb1496a78547
jafow <jared.a.fowler@gmail.com>
2016-07-10 00:51:57 -0700

    update entry to use .js

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path')
 
 
 module.exports = {
-  entry: path.join(__dirname, 'src/app.jsx'),
+  entry: path.join(__dirname, 'src/app.js'),
   output: {
     path: path.join(__dirname, 'client/'),
     filename: 'bundle.js'
