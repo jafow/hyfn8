6d079f0a71df6250cfd69e08917bc74ff264152e
jafow <jared.a.fowler@gmail.com>
2016-07-11 01:01:35 -0700

    cleanup

--- server.js
+++ server.js
@@ -6,15 +6,13 @@ const fs = require('fs')
 const PORT = process.env.PORT || 3000
 
 app.use(express.static('client'))
-// app.use(bodyParser.json())
+app.use(bodyParser.json())
 
 app.get('/ads', (req, res) => {
-  console.log('getting ads')
   res.sendFile(path.join(__dirname, 'data/ads.js'))
 })
 
 app.get('/ads_metrics', (req, res) => {
-  console.log('getting ads metrs')
   res.sendFile(path.join(__dirname, 'data/ads_metrics.js'))
 })
 
